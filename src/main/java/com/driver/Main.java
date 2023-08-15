f669201948b699b8eef8e27a5f50cb81b64c8bdb
Sahil Dongre <sahildongre1000@gmail.com>
2023-08-15 11:54:32 +0530

    poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,17 +1,4 @@
 package com.driver;
-class Product{
-    public int product(int x, int y ){
-        return x+y;
-    }
-    public int product(int x, int y ,int z){
-        return x+y+z;
-    }
-
-    public double product(double x, double y) {
-        return x+y;
-    }
-
-}
 
 
 public class Main {
