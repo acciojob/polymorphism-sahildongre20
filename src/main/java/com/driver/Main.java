bc0d9a640a638054d45d298656d5aba8b9004118
Sahil Dongre <sahildongre1000@gmail.com>
2023-08-15 10:41:17 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
+class Product{
+    public int product(int x, int y ){
+        return x+y;
+    }
+    public int product(int x, int y ,int z){
+        return x+y+z;
+    }
+
+    public double product(double x, double y) {
+        return x+y;
+    }
+
+}
+
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,9);
+        p.product(5,9,3);
+        p.product(5.1,9.2);
+
+    }
 
 }
